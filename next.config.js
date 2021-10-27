1c863eea50a7004f8feea0668f537bc52f83edee
Jesdakorn Saelor <jesdakorns@gmail.com>
2021-10-28 02:26:04 +0700

    add

--- next.config.js
+++ next.config.js
@@ -5,16 +5,22 @@ module.exports = {
     HOST_BASE: 'https://jesdakorn.herokuapp.com/',
     API_BASE: 'http://127.0.0.1:8000/',
     API_HOST: 'http://127.0.0.1:8000/api/',
-},
+  },
 }
 
 
-const withPWA = require("next-pwa");
+// const withPWA = require("next-pwa");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-});
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   publicRuntimeConfig: {
+//     HOST_BASE: 'https://jesdakorn.herokuapp.com/',
+//     API_BASE: 'http://127.0.0.1:8000/',
+//     API_HOST: 'http://127.0.0.1:8000/api/',
+//   },
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   },
+// });
